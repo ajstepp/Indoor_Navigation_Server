ac98856f453f3fd7aba60098cdd64b266e73835e
brendancieslik <50969897+brendancieslik@users.noreply.github.com>
2020-10-29 07:31:51 -0400

    Add files via upload

---
+++ Web Client/mapCreator.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Indoor Navigation Server-</title>
+    <!-- Bootstrap CSS file, along with other bootstrap and CSS files-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/adjustments.css">
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavBar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img src="logo.png"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="index.php">About Us</a></li>
+                    <li class="active"><a href="mapCreator.php">Map Creator</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="mapCreator">
+
+    <div class="container map-creator" id="aboutUs">
+        <div class="row">
+            <div class="col-lg-12">
+                <a id="aboutUs"><h4>Select your Map</h4></a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    </div>
+</body>
+</html>
