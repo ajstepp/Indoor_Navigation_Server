5d9c6b214cb25dabdf12721e0d103ee5b32b98a7
Andrew Stepp <hockeyfan921@gmail.com>
2020-11-29 19:40:59 -0500

    Add files via upload

--- Mapping/ToJSON.js
+++ Mapping/ToJSON.js
@@ -7,17 +7,25 @@ document.getElementById('save').addEventListener('click', function () {
         image: canvasContents,
         date: Date.now()
     };
-    var string = JSON.stringify(data);
-
+    
+    var map = JSON.stringify(data);
+    var map = map + "\n";
+    
+    var points = "";
+    
+    for (var i=0; i<Stored_Coordinates.length;i++) {
+        points += JSON.stringify(Stored_Coordinates[i]);
+        points += "\t";
+    }
     // create a blob object representing the data as a JSON string
-    var file = new Blob([string], {
-        type: 'application/json'
+    var file = new Blob([map, points], {
+        type: 'application/json',
     });
-
+    
     // trigger a click event on an <a> tag to open the file explorer
     var a = document.createElement('a');
     a.href = URL.createObjectURL(file);
-    a.download = 'data.json';
+    a.download = 'Completed_Map.json';
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
