ebe797ddf9e054c3b867912da6dd34e0e26b952c
Connor <connormadek@gmail.com>
2020-12-02 17:59:50 -0500

    maybe?

--- Web Client/README.txt
+++
@@ -1,1 +0,0 @@
-This folder will be used to host any web-based code that will serve as the front end of our server
--- Web Client/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();    //reset session variables
-session_destroy();  //end session
-header("location: login.php");  //send back to login page
-exit;
-?>
